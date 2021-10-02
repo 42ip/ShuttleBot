6ba0b8c1e95e41c785409aa854c8eded11cf78c2
Prash2002 <1707prash@gmail.com>
2021-10-03 03:20:14 +0530

    earth command added

--- shuttleBot.py
+++ shuttleBot.py
@@ -1,30 +1,30 @@
 import discord
-import json,sys,os,random
+import os
+import random
 import requests
-
-
+import io
+from PIL import Image, ImageDraw
 
 apiKey = os.environ.get('apiKey')
 token = os.environ.get('token')
 
+
 class MyClient(discord.Client):
     global apiKey
+
     async def on_ready(self):
         print('Logged in as')
         print(self.user.name)
         print(self.user.id)
         print('------')
 
+    possibleIntros = ['Hi, My name is Shuttle. Hope I can get some space here', "Hello, I'm Shuttle! I am tasked to orbit this server",
+                      "Hey, My name is Shuttle. My sensors are telling me that you might be a star", "Hello, I am Shuttle. You seem to radiate some good vibes"]
 
-
-    possibleIntros = ['Hi, My name is Shuttle. Hope I can get some space here', "Hello, I'm Shuttle! I am tasked to orbit this server", "Hey, My name is Shuttle. My sensors are telling me that you might be a star", "Hello, I am Shuttle. You seem to radiate some good vibes"]
-    
-    
-    
     async def on_message(self, message):
         if message.author.id == self.user.id:
             return
-    
+
         if message.content.startswith('>hello'):
             await message.reply(random.choice(self.possibleIntros), mention_author=True)
 
@@ -32,8 +32,9 @@ class MyClient(discord.Client):
             chan = message.channel
             arr = []
             while arr == []:
-                num = random.randint(1,3250)
-                response = requests.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol={}&camera=navcam&api_key={}'.format(str(num),apiKey))
+                num = random.randint(1, 3250)
+                response = requests.get(
+                    'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol={}&camera=navcam&api_key={}'.format(str(num), apiKey))
                 if response.status_code == 200:
                     vals = response.json()
                     arr = vals['photos']
@@ -44,15 +45,43 @@ class MyClient(discord.Client):
 
         if message.content.startswith('>apod'):
             chan = message.channel
-            response = requests.get('https://api.nasa.gov/planetary/apod?api_key={}'.format(apiKey))
+            response = requests.get(
+                'https://api.nasa.gov/planetary/apod?api_key={}'.format(apiKey))
             if response.status_code == 200:
                 vals = response.json()
                 await chan.send("Today's Title is: " + vals['title'])
                 await chan.send(vals['url'])
 
+        if message.content.startswith('>earth'):
+            if len(message.mentions) == 0:
+                person = message.author
+            else:
+                person = message.mentions[0]
+            print(person.avatar_url)
+            response = requests.get(person.avatar_url)
+            image_bytes = io.BytesIO(response.content)
+            im2 = Image.open(image_bytes)
+            response = requests.get(
+                'https://cdn.mos.cms.futurecdn.net/3upZx2gxxLpW7MBbnKYQLH-1200-80.jpg')
+            image_bytes = io.BytesIO(response.content)
+            im1 = Image.open(image_bytes)
+            im2 = im2.resize((440, 440))
+            mask_im = Image.new("L", im2.size, 0)
+            draw = ImageDraw.Draw(mask_im)
+            draw.ellipse((0, 0, im2.width, im2.height), fill=170)
+            im = im1.copy()
+            im.paste(im2, (55, 65), mask_im)
+            # im = im2.copy()
+            with io.BytesIO() as image_binary:
+                im.save(image_binary, 'PNG')
+                image_binary.seek(0)
+                picture = discord.File(image_binary, "space.png")
+                await message.channel.send("Guys i am earth now <:sadge:886538902352068628>")
+                await message.channel.send(file=picture)
+
         if message.content.startswith('>help'):
             commands = ["1. hello : Know the bot",
-            "2. apod : Astronomical Picture Of The Day" , "3. mars : NAVCAM picture from planet Mars"]
+                        "2. apod : Astronomical Picture Of The Day", "3. mars : NAVCAM picture from planet Mars", "4. earth: you become earth"]
             msg = "Hey, heard a SOS! Here's all you need to know: \n Prefix : > \n"
             for val in commands:
                 msg += val + '\n'
