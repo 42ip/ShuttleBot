b455e7052077a439f369412094a663e41d694126
TheNova22 <jayantsogikar@gmail.com>
2021-10-10 02:29:16 +0530

    Added a place image feature

--- shuttleBot.py
+++ shuttleBot.py
@@ -8,7 +8,7 @@ import gpt_2_simple as gpt2
 import tarfile
 import gdown
 import sys
-
+import geograpy
 
 url = 'https://drive.google.com/uc?id=1EVvLwJA1f507iF1fteBOZaJUK6CbBvl-'
 output = 'checkpoint_run1.tar'
@@ -163,6 +163,35 @@ class MyClient(discord.Client):
                             await chan.send('Here is a photo of {} that got sent in my family satellite group :satellite_orbital:'.format(genreName[vals[1]]))
                             await chan.send(supreme['urls']['small'])
 
+
+            elif resp.startswith('tbd'):
+                placeName = geograpy.get_place_context(text=resp.split()[1])
+                if len(placeName.countries) > 0:
+                    response = requests.get(
+                        'https://api.unsplash.com/photos/random/?client_id={}&query={}'.format(splashKey,placeName.countries[0]))
+                    if response.status_code == 200:
+                        vals = response.json()
+                        await chan.send('Here is a photo from the country {} for you :wink:'.format(placeName.countries[0]))
+                        await chan.send(vals['urls']['small'])
+
+                elif len(placeName.cities) > 0:
+                    response = requests.get(
+                        'https://api.unsplash.com/photos/random/?client_id={}&query={}'.format(splashKey,placeName.cities[0]))
+                    if response.status_code == 200:
+                        vals = response.json()
+                        await chan.send('Here is a photo from the country {} for you :wink:'.format(placeName.cities[0]))
+                        await chan.send(vals['urls']['small'])
+                
+                elif len(placeName.regions) > 0:
+                    response = requests.get(
+                        'https://api.unsplash.com/photos/random/?client_id={}&query={}'.format(splashKey,placeName.regions[0]))
+                    if response.status_code == 200:
+                        vals = response.json()
+                        await chan.send('Here is a photo from the country {} for you :wink:'.format(placeName.regions[0]))
+                        await chan.send(vals['urls']['small'])
+                
+                else:
+                    await message.reply("A man who is said to be from the ocean stole some of our drives, maybe this unknown place's info was in it <:sadge:886538902352068628>")
 
 
             else:
