ecdac2ed6420247931fa91f7111cb7847ba49421
TheNova22 <jayantsogikar@gmail.com>
2022-10-01 02:50:01 +0530

    Leetcode

--- shuttleBot.py
+++ shuttleBot.py
@@ -13,6 +13,7 @@ from commands.hello import hello
 from commands.help import help as helper
 from commands.mars import mars
 from commands.splash import splash
+import json
 
 nltk.download('all')
 found = False
@@ -65,6 +66,10 @@ class MyClient(discord.Client):
             if resp == 'hello':
                 await hello(introList=self.possibleIntros,channel=chan)
 
+            elif resp == 'prob':
+                r = requests.post("https://leetcode.com/graphql",  json = {"query" : "\n    query questionOfToday {\n  activeDailyCodingChallengeQuestion {\n    date\n    userStatus\n    link\n    question {\n      acRate\n      difficulty\n      freqBar\n      frontendQuestionId: questionFrontendId\n      isFavor\n      paidOnly: isPaidOnly\n      status\n      title\n      titleSlug\n      hasVideoSolution\n      hasSolution\n      topicTags {\n        name\n        id\n        slug\n      }\n    }\n  }\n}\n    "})
+                link = "https://leetcode.com" + json.loads(r.text)["data"]["activeDailyCodingChallengeQuestion"]["link"]
+                await chan.send("Here is a question for you to solve from Leetcode! \n" + link)
 
             elif resp == 'mars':
                 await mars(channel=chan,apiKey=apiKey)
