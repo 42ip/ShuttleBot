dec96cd70ffac1668a58f825198cc5a2946ccab3
TheNova22 <jayantsogikar@gmail.com>
2022-10-01 22:25:03 +0530

    Converted LC function into seperate file

--- shuttleBot.py
+++ shuttleBot.py
@@ -9,6 +9,7 @@ import gdown
 import geograpy
 import nltk
 from commands.apod import apod
+from commands.lc import prob
 from commands.earth import earth
 import commands.getCie as getCie
 from commands.hello import hello
@@ -75,9 +76,7 @@ class MyClient(discord.Client):
                 await hello(introList=self.possibleIntros,channel=chan)
 
             elif resp == 'prob':
-                r = requests.post("https://leetcode.com/graphql",  json = {"query" : "\n    query questionOfToday {\n  activeDailyCodingChallengeQuestion {\n    date\n    userStatus\n    link\n    question {\n      acRate\n      difficulty\n      freqBar\n      frontendQuestionId: questionFrontendId\n      isFavor\n      paidOnly: isPaidOnly\n      status\n      title\n      titleSlug\n      hasVideoSolution\n      hasSolution\n      topicTags {\n        name\n        id\n        slug\n      }\n    }\n  }\n}\n    "})
-                link = "https://leetcode.com" + json.loads(r.text)["data"]["activeDailyCodingChallengeQuestion"]["link"]
-                await chan.send("Here is a question for you to solve from Leetcode! \n" + link)
+                await prob(chan)
 
             elif resp == 'mars':
                 await mars(channel=chan,apiKey=apiKey)
